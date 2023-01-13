d5c9c70d5784d0303808f70b0502519852c96593
michaelmirzakandov <mtrix92@gmail.com>
2023-01-13 14:10:48 -0500

    front end structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+lists = List.create([{name: 'Grocery List'},{name: 'To Do List'}])
+Item.create(name: 'milk', list_id: 1)
